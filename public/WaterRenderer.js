4b5b0c16d08da40e2425d02faf3c798da91111bc
Collin <collin2985@gmail.com>
2025-09-27 18:21:00 +0100

    textures

--- public/WaterRenderer.js
+++ public/WaterRenderer.js
@@ -144,7 +144,7 @@ vec2 foamUv = fract(vUv * 25.0 + vec2(u_time * 0.015, u_time * 0.009));
         
 // Depth-based color and transparency
 float shallowFactor    = clamp(depth / 0.4, 0.0, 1.0);
-float transitionFactor = smoothstep(0.0, 0.3, depth);
+float transitionFactor = smoothstep(0.0, 0.2, depth);
 
 vec3 waterBaseColor = mix(u_shallow_color.rgb, u_deep_color.rgb, transitionFactor);
 
